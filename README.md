011ce1f78766dba11a0aa05487227b416df92cb6
Phil Balchin <phil@maniacalrobot.co.uk>
2024-06-27 20:48:39 +0100

    Added set 1 structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cryptopals
-Solutions to cryptopals cryptography challenges
+Solutions to cryptopals cryptography challenges: https://cryptopals.com
+
+## Running
+
+Solutions are run as Ruby RSpec examples:
+
+```
+rspec set_1_basics.rb
+```
---
+++ set_1_basics.rb
@@ -0,0 +1,35 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'base64'
+  gem 'json'
+  gem 'rspec'
+end
+
+RSpec.describe "Crypto Challenges Set 1" do
+
+  describe "Challenge 1: Convert hex to base64" do
+  end
+
+  describe "Challenge 2: Fixed XOR" do
+  end
+
+  describe "Challenge 3: Single-byte XOR cipher" do
+  end
+
+  describe "Challenge 4: Detect single-character XOR" do
+  end
+
+  describe "Challenge 5: Implement repeating-key XOR" do
+  end
+
+  describe "Challenge 6: Break repeating-key XOR" do
+  end
+
+  describe "Challenge 7: AES in ECB mode" do
+  end
+
+  describe "Challenge 8: Detect AES in ECB mode" do
+  end
+end
